9e85b1cda40640f583a45fb2595470bcf13aca33
gitwamoto <iwamoto.yukiharu.mc@ehime-u.ac.jp>
2023-05-17 20:41:38 +0900

    Update matplotlibwx.py

--- matplotlibwx.py
+++ matplotlibwx.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 # matplotlibwx.py
 # by Yukiharu Iwamoto
-# 2023/5/17 8:30:33 PM
+# 2023/5/17 8:41:24 PM
 
 # Macの場合，文字入力後に引用符が勝手に変わったりしてうまく動かない．
 # 「システム環境設定」→「キーボード」→「ユーザー辞書」→「スマート引用符とスマートダッシュを使用」のチェックを外す．
 
-version = '2023/5/17 8:30:33 PM'
+version = '2023/5/17 8:41:24 PM'
 
 import os
 languages = os.environ.get('LANG')
@@ -40,7 +40,7 @@ import xlrd
 import openpyxl
 import csv
 from PIL import Image, ImageChops
-import wx # python 2 -> pip install wxPython==4.1.0
+import wx # pip install wxPython==4.1.0
 import wx.grid
 import requests
 import zipfile
