3d7493dc165180ef350b07370de1b44005b6cea7
gitwamoto <iwamoto.yukiharu.mc@ehime-u.ac.jp>
2024-06-12 12:41:02 +0900

    Update matplotlibwx.py

--- matplotlibwx.py
+++ matplotlibwx.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 # matplotlibwx.py
 # by Yukiharu Iwamoto
-# 2024/6/12 12:34:44 PM
+# 2024/6/12 12:40:49 PM
 
 # Macの場合，文字入力後に引用符が勝手に変わったりしてうまく動かない．
 # 「システム環境設定」→「キーボード」→「ユーザー辞書」→「スマート引用符とスマートダッシュを使用」のチェックを外す．
 
-version = '2024/6/12 12:34:44 PM'
+version = '2024/6/12 12:40:49 PM'
 
 import os
 languages = os.environ.get('LANG')
@@ -504,6 +504,7 @@ def data_from_equation(equation, param_dict = None):
     if param_dict is None:
         param_dict = {}
     param_dict['math'] = globals()['math']
+    equation = equation.replace('^', '**')
     r = pat_eq_plot.match(equation)
     eq = pat_math.sub(r'math.\1', r[1])
 
